127325ed14eec4c464232ea148de171215273777
kalpana-paawr <pawarkalpana1929@gmail.com>
2025-08-26 22:59:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,7 @@
 filename=input("enter the filename":)
    if filename.endwiths('.csv'):
-     print('csv file processing')
-else:
-  print('pdf files processing')
+       print('csv file processing')
+   elif filename.startwiths('demo'):
+       print("Thise file is avalible")
+  else:
+       print('pdf files processing')
